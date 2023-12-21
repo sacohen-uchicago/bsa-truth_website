cb0cb1bd3fbb1314c2b41ff582bba3722b3a0b8f
sacohen <sacohen@uchicago.edu>
2023-12-21 17:20:40 +0000

    Route Route: Added root route to 'routes.rb' file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
   get("/home", { :controller => "pages", :action => "home" })
   get("/founders", { :controller => "pages", :action => "founders" })
   get("/contact", { :controller => "pages", :action => "contact" })
