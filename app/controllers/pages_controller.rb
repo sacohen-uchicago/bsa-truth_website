a3d4eff4caff3592bf5ecbcc8734376cc3a4c64e
sacohen <secohen135@gmail.com>
2023-12-21 16:53:39 +0000

    Working Website: Fixed the code form the old website and now it works on the server.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,13 @@
 class PagesController < ApplicationController
   def home
-    render({ :template => "page_template/home.html.erb" })
+    render({ :template => "page_template/home" })
   end
 
   def founders
-    render({ :template => "page_template/founders.html.erb" })
+    render({ :template => "page_template/founders" })
   end
 
   def contact
-    render({ :template => "page_template/contact.html.erb" })
+    render({ :template => "page_template/contact" })
   end
 end
