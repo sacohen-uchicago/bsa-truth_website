ee486bd0a99f2a7ba20fbc1779a31eb0813fda51
Seth Cohen <seth.cohen@chicagobooth.edu>
2023-12-27 06:40:57 +0000

    Post: Added post to the authenticator

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
 
-  skip_before_action(:authenticate_user!, { :only => [:home, :founders, :contact, :blog] })
+  skip_before_action(:authenticate_user!, { :only => [:home, :founders, :contact, :post] })
 
   def home
     render({ :template => "page_template/home" })
